0853d74b8de58c615ca1115e3e25647e52ec3ef4
Chris Gadzinski <zenmags@gmail.com>
2015-09-21 17:43:44 +0000

    stalling work, there are more productive things to do in the context of calculator development

--- src/Hasio.hs
+++ src/Hasio.hs
@@ -1,4 +1,10 @@
 module Hasio
-    ( module Hasio.Application ) where
+    (
+    -- * Application representation
+    module Hasio.Application
+    -- * BASIC language representation and evaluation
+    , module Hasio.Lang.Basic
+    ) where
 
 import Hasio.Application
+import Hasio.Lang.Basic
--- src/Hasio/Lang/Basic.hs
+++ src/Hasio/Lang/Basic.hs
@@ -1,16 +1,26 @@
+-- |
 module Hasio.Lang.Basic
-    ( Expr, Command, Program
+    (
+    -- * AST
+    Expr, Command, Program
+    -- * Parser
+    , parseCommand
+    , parseProgram
     ) where
 
-newtype NumVar = NumVar Char deriving (Eq)
-newtype Label = Label Int deriving (Eq)
+import Data.List
+
+-- * AST
+
+newtype NumVar = NumVar Char deriving (Eq, Show)
+newtype Label = Label Int deriving (Eq, Show)
 
 data Expr =
     LiteralInt -- ^ an integer
     | RefNumVar NumVar -- ^ references a numeric var
     | Add Expr Expr
     | Multiply Expr Expr
-    | Power Expr Expr
+    | Power Expr Expr deriving (Eq, Show)
 
 data Command =
     Empty -- ^ Empty, does nothing.
@@ -24,6 +34,44 @@ data Command =
     | DefElse -- ^ Begins the else branch of an if statement.
     | DefIfEnd -- ^ Ends an if statement.
 
-    | AssignVar Expr NumVar
+    | AssignVar Expr NumVar deriving (Eq, Show)
 
 type Program = [Command]
+
+-- * Parser
+
+type Location = Int
+
+parseFail :: Location -> String -> Either a String
+parseFail loc str = Right . concat $
+    [ "BASIC parse error: " , str, " (line ", show loc, ")"]
+
+parseLabel :: String -> Maybe Label
+parseLabel [x] = Label . snd <$> find ((== x) . fst) (zip ['0'..'9'] [0..9])
+parseLabel _ = Nothing
+
+parseExpr :: String -> Maybe Expr
+parseExpr str =
+    case str of
+        '(':xs -> undefined
+        _ -> undefined
+    where
+        toNextParens =
+            foldr ()
+
+parseCommand :: Location -> String -> Either Command String
+parseCommand loc str =
+    case words str of
+        ["Lbl", labelStr] -> case parseLabel labelStr of
+            Nothing -> parseFail loc "bad label identifier in DefLabel"
+            Just label -> Left $ DefLabel label
+        ["Goto", labelStr] -> case parseLabel labelStr of
+            Nothing -> parseFail loc "bad label identifier in GotoLabel"
+            Just label -> Left $ GotoLabel label
+        ["If", _] -> case parseExpr (unwords . tail . words $ str) of
+            Nothing -> parseFail loc "bad predicate expression in DefIfBegin"
+            Just expr -> Left $ DefIfBegin expr
+        _ -> parseFail loc "bad command"
+
+parseProgram :: String -> Either Program String
+parseProgram str = Right "haven't implemented yet"
